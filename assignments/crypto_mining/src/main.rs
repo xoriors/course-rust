474260f0b237fed8f0922221b3e8e431ce25ab21
Nastasie Raul-Ionut <ion_nastasie@yahoo.com>
2025-02-25 21:29:31 +0200

    dynamic weights

--- assignments/crypto_mining/src/main.rs
+++ assignments/crypto_mining/src/main.rs
@@ -2,6 +2,7 @@ use std::io;
 use rand::Rng;
 use rand::distributions::WeightedIndex;
 use rand::distributions::Distribution;
+use std::collections::HashMap;
 
 const BILLION: u64 = 1_000_000_000;
 
@@ -27,25 +28,36 @@ fn main() {
             let mut rng = rand::thread_rng();
 
             let mut array:Vec<u64> = vec![0; n];
-            let mut weights:Vec<u64> = vec![0; (BILLION / 100) as usize];
+            // let mut weights:Vec<u64> = vec![0; (BILLION / 100) as usize];
+            // let mut weights:Vec<u64, u8> = vec![(0, 0); 1];
+            let mut weight_map = HashMap::new();
 
             for i in 0..n {
                 array[i] = rng.gen_range(0..BILLION);
-                weights[(array[i] / 100) as usize] += 1;
+                if weight_map.contains_key(&(array[i] / 100)) {
+                    *weight_map.get_mut(&(array[i] / 100)).unwrap() += 1;
+                } else {
+                    weight_map.insert(array[i] / 100, 1);
+                }
             }
 
-            let mut dist = WeightedIndex::new(&weights).unwrap();
+            let mut keys:Vec<u64> = weight_map.clone().into_keys().collect();
+            let mut weights:Vec<u64> = weight_map.into_values().collect();
+
+            // println!("{:?} \n {:?}", keys, weights);
+
 
             // println!("Weights: {dist:?}");
-
             // println!("Array: {array:?}");
 
             let mut counter = 0;
 
             loop {
+                let mut dist = WeightedIndex::new(&weights).unwrap();
                 counter += 1;
-                // let num = rand::thread_rng().gen_range(0..BILLION);
-                let mut num: u64 = (dist.sample(&mut rng) * 100 + rng.gen_range(0..100)).try_into().unwrap();
+                let random_index = dist.sample(&mut rng);
+                // println!("{random_index}");
+                let mut num: u64 = (keys[random_index] * 100 + rng.gen_range(0..100)).try_into().unwrap();
 
                 // small hack -> weighted gen pt num / 100 + random gen 0-100
 
@@ -56,9 +68,11 @@ fn main() {
                     if array.is_empty() {
                         break;
                     }
-                    // let weight_index: usize = (num / 100).try_into().unwrap();
-                    weights[(num / 100) as usize] -= 1;
-                    dist.update_weights(&[((num / 100).try_into().unwrap(), &(weights[(num / 100) as usize]))]).unwrap();
+                    weights[random_index] -= 1;
+                    if weights[random_index] == 0 {
+                        weights.remove(random_index);
+                        keys.remove(random_index);
+                    }
                 }
             }
 
@@ -71,6 +85,8 @@ fn main() {
             }
 
             iter_result.avg += counter as f64;
+
+            // break;
         }
         println!("Min: {}, Max: {}, Avg: {}", iter_result.min, iter_result.max, iter_result.avg / 100.0);
     }
