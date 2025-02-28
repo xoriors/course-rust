cbf30cee14e5d69f89c57fe042dcd148d19cd6cc
Eyob <eyobIborgI@gmail.com>
2025-02-28 23:05:22 +0300

    Added criterion benchmarks for both and generated graphs comparing the two

--- Chapter_1-3/benches/my_benchmark/main.rs
+++ Chapter_1-3/benches/my_benchmark/main.rs
@@ -1,16 +1,36 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion, BenchmarkId};
+use chapter_1_3::weighted::wei_dist::weighted_distribution;
+use chapter_1_3::uniform::uni_dist::uniform_distribution;
 
-fn fibonacci(n: u64) -> u64 {
-    match n {
-        0 => 1,
-        1 => 1,
-        n => fibonacci(n-1) + fibonacci(n-2),
+fn bench_distributions(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Distribution Comparison");
+
+    let mut extracted_array_u64 = [123, 235, 633, 677, 888, 324];
+    let mut extracted_array_f32 = [ 0.54633904, 0.506454, 0.10968602, 0.24843967, 0.4981829, 0.43807256];
+    let matched = [false; 100];
+    let success = 0;
+    let user_input = 6;
+
+    for &loop_amount in &[10u64, 100u64, 1000u64] {
+        group.bench_with_input(
+            BenchmarkId::new("WEIGHTED-DISTRIBUTION", loop_amount),
+            &loop_amount,
+            |b, &loop_amount| {
+                b.iter(|| weighted_distribution(loop_amount, &mut extracted_array_u64, matched))
+            },
+        );
+
+        group.bench_with_input(
+            BenchmarkId::new("UNIFORM-DISTRIBUTION", loop_amount),
+            &loop_amount,
+            |b, &loop_amount| {
+                b.iter(|| uniform_distribution(loop_amount, user_input, &mut extracted_array_f32, success, matched))
+            },
+        );
     }
+
+    group.finish();
 }
 
-fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("Benchmarking Loops: ", |b| b.iter(|| fibonacci(black_box(20))));
-}
-
-criterion_group!(benches, criterion_benchmark);
+criterion_group!(benches, bench_distributions);
 criterion_main!(benches);
